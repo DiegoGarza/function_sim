d6112ba75bc89c2e746efe783cf52905dc93be1a
DiegoGarza <dgarza.m93@gmail.com>
2015-03-23 13:01:07 -0600

    Initial Commit

---
+++ function_sim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  function_sim
+//
+//  Created by Diego Garza on 3/23/15.
+//  Copyright (c) 2015 Diego Garza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ function_sim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  function_sim
+//
+//  Created by Diego Garza on 3/23/15.
+//  Copyright (c) 2015 Diego Garza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
